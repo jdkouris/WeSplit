6a693bdd5b4a4c6eb64e3ad4ba6cb96cd41a60be
John Kouris <jdkouris@gmail.com>
2019-11-27 09:55:43 -0800

    Calculated the total per person MVP complete

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,6 +13,18 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double {
+        // calculate the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView {
@@ -37,7 +49,7 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Text("$\(checkAmount)")
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }.navigationBarTitle("WeSplit")
         }
