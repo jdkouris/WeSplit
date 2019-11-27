92e92988a55f08e6437b005073c758f88be216dd
John Kouris <jdkouris@gmail.com>
2019-11-27 08:57:44 -0800

    Modified program state for the tapCount button and @State

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,18 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Text("Hello World")
-                }
-            }
-//        .navigationBarTitle(Text("SwiftUI"))
-                
-                // generates the small title
-//                .navigationBarTitle("SwiftUI", displayMode: .inline)
-            .navigationBarTitle("SwiftUI")
+        Button("Tap Count: \(tapCount)") {
+            self.tapCount += 1
         }
     }
 }
