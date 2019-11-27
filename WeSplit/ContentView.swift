80b3e73b909ad71bb746d4daa26e1e8c825eed8f
John Kouris <jdkouris@gmail.com>
2019-11-27 08:15:20 -0800

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by John Kouris on 11/27/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
